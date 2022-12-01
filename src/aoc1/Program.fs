6f90b1dfc6faf5fc2bdc99c05ea17ec2a14ab475
Émile <emileschneider7@gmail.com>
2022-12-01 21:22:46 +0100

    Solved aoc22 1

---
+++ src/aoc1/Program.fs
@@ -0,0 +1,25 @@
+﻿open System
+open System.IO
+
+
+let text = File.ReadAllText(@"/home/emile/recreational_programming/aoc22/src/aoc1/input")
+
+let lines = text.Split("\n")
+
+
+let elves =
+    ([[]], lines)
+    ||> Array.fold (fun (acc: list<list<string>>) (line: string) ->
+                    match line.Length with
+                    | 0 -> [] :: acc
+                    | _ -> (line :: acc.Head) :: acc)
+
+let calories = elves |> List.map (fun l -> l |> List.map (fun e -> Int32.Parse(e)))
+
+let maxcalories = calories |> List.map (fun l -> l |> List.sum)
+
+
+[<EntryPoint>]
+let main argv =
+    printfn "max: %i" (maxcalories |> List.max)
+    0
