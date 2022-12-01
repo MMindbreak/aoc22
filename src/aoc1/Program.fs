6452c539ee41b1943e78d0bbb4cbbdc7a5729860
Émile <emileschneider7@gmail.com>
2022-12-01 21:25:16 +0100

    added some comments

--- src/aoc1/Program.fs
+++ src/aoc1/Program.fs
@@ -6,7 +6,7 @@ let text = File.ReadAllText(@"/home/emile/recreational_programming/aoc22/src/aoc
 
 let lines = text.Split("\n")
 
-
+// fold the array into a list<list<string>>, where each list<string> correspondes to an elve
 let elves =
     ([[]], lines)
     ||> Array.fold (fun (acc: list<list<string>>) (line: string) ->
@@ -14,8 +14,10 @@ let elves =
                     | 0 -> [] :: acc
                     | _ -> (line :: acc.Head) :: acc)
 
+// parse the strings to ints
 let calories = elves |> List.map (fun l -> l |> List.map (fun e -> Int32.Parse(e)))
 
+// sum to get the total calories of each elve
 let maxcalories = calories |> List.map (fun l -> l |> List.sum)
 
 
