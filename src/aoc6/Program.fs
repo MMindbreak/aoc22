1e076357a8a1b600522710e9efd6e0b39c97a03e
Emile Schneider <emileschneider7@gmail.com>
2022-12-07 16:43:10 +0100

    part 2 done

--- src/aoc6/Program.fs
+++ src/aoc6/Program.fs
@@ -12,12 +12,14 @@ let getStarterPackagePosition (windowed: list<array<char>>) =
     let rec rf count (windowed: list<array<char>>) =
         let h = windowed.Head
         let t = windowed.Tail
-        for c in h do
-            Console.Write(c)
-        Console.Write('\n')
+
         match windowed with
         | [] -> -1
-        | head :: tail -> if areAllUnique head then count + head.Length else rf (count + 1) tail
+        | head :: tail ->
+            if areAllUnique head then
+                count + head.Length
+            else
+                rf (count + 1) tail
 
     rf 0 windowed
 
@@ -26,6 +28,6 @@ let getStarterPackagePosition (windowed: list<array<char>>) =
 let main argv =
 
     let input = File.ReadAllText("/home/emile/programming/aoc22/src/aoc6/input")
-    let windowed = input |> Seq.windowed 4 |> Seq.toList
+    let windowed = input |> Seq.windowed 14 |> Seq.toList
     printfn "start position %i" (getStarterPackagePosition windowed)
     0
